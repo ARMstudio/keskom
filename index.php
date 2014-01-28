e7bd887e2e0e797db308e1f19bfc9d439d98975a
rismunandar <rswnt72@gmail.com>
2014-01-29 04:52:51 +0700

    tes baru
    index baru

--- index.php
+++ index.php
@@ -2,10 +2,18 @@
 <html>
 <body>
 
-<form>
-	<input type="radio" name="sex" value="male">Male<br>
-	<input type="radio" name="sex" value="female">Female
+<form method="post" name="myform" action="readme2.txt">
+	<p id="p">Soallllll</p>
+	<input type="radio" name="a" value="male" onclick="cthFungsi()">A<br>
+	<input type="radio" name="b" value="female">B<br>
+	<input type="radio" name=" " value="female">Kosong<br>
 </form> 
 
+<script>
+	function cthFungsi(){
+		document.getElementById("p").innerHTML="Hello World";
+		document.myform.submit();
+	}
+</script>
 </body>
 </html> 
