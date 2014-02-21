d00d668bc676848c01907166b6fd699fe0f85452
rismunandar <rswnt72@gmail.com>
2014-02-21 07:27:57 +0700

    layout
    ini layout nya ndre

---
+++ layout/index.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Compiler 2013 | Quesscomp</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/extends.css" rel="stylesheet">
+	<style>
+		
+	</style>
+</head>
+<body>
+
+	<div class="container kertas">
+		<div class="row">
+		  <div class="col-md-6 gambar">
+			<img src="images/logo.png" alt="compiler2014">
+		  </div>
+		  <div class="col-md-6 quesscomp">quesscomp 2014</div>
+		</div>
+		<hr>
+		<div class="row">
+		  <div class="col-md-12">
+			<div class="row soal">
+			
+			<ol>
+				<li>Siapakah nama saya ?
+					<form action="" method="post">
+						<ul>
+							<li><input type="radio" name="sex" value="male">Male</li>
+							<li><input type="radio" name="sex" value="male">Male</li>
+							<li><input type="radio" name="sex" value="male">Male</li>
+							<li><input type="radio" name="sex" value="male">Male</li>
+						</ul>
+					</form>
+				</li>
+				<br>
+				<li>Siapakah nama saya ?
+					<form action="" method="post">
+						<ul>
+							<li><input type="radio" name="sex" value="male">Male</li>
+							<li><input type="radio" name="sex" value="male">Male</li>
+							<li><input type="radio" name="sex" value="male">Male</li>
+							<li><input type="radio" name="sex" value="male">Male</li>
+						</ul>
+					</form>
+				</li>
+				<br>
+				<li>Siapakah nama saya ?
+					<form action="" method="post">
+						<ul>
+							<li><input type="radio" name="sex" value="male">Male</li>
+							<li><input type="radio" name="sex" value="male">Male</li>
+							<li><input type="radio" name="sex" value="male">Male</li>
+							<li><input type="radio" name="sex" value="male">Male</li>
+						</ul>
+					</form>
+				</li>
+				<br>
+				<li>Siapakah nama saya ?
+					<form action="" method="post">
+						<ul>
+							<li><input type="radio" name="sex" value="male">Male</li>
+							<li><input type="radio" name="sex" value="male">Male</li>
+							<li><input type="radio" name="sex" value="male">Male</li>
+							<li><input type="radio" name="sex" value="male">Male</li>
+						</ul>
+					</form>
+				</li>
+				<br>
+				<li>Siapakah nama saya ?
+					<form action="" method="post">
+						<ul>
+							<li><input type="radio" name="sex" value="male">Male</li>
+							<li><input type="radio" name="sex" value="male">Male</li>
+							<li><input type="radio" name="sex" value="male">Male</li>
+							<li><input type="radio" name="sex" value="male">Male</li>
+						</ul>
+					</form>
+				</li>
+				<br>
+				<li>Siapakah nama saya ?
+					<form action="" method="post">
+						<ul>
+							<li><input type="radio" name="sex" value="male">Male</li>
+							<li><input type="radio" name="sex" value="male">Male</li>
+							<li><input type="radio" name="sex" value="male">Male</li>
+							<li><input type="radio" name="sex" value="male">Male</li>
+						</ul>
+					</form>
+				</li>
+				<br>
+				<li>Siapakah nama saya ?
+					<form action="" method="post">
+						<ul>
+							<li><input type="radio" name="sex" value="male">Male</li>
+							<li><input type="radio" name="sex" value="male">Male</li>
+							<li><input type="radio" name="sex" value="male">Male</li>
+							<li><input type="radio" name="sex" value="male">Male</li>
+						</ul>
+					</form>
+				</li>
+				<br>
+				<li>Siapakah nama saya ?
+					<form action="" method="post">
+						<ul>
+							<li><input type="radio" name="sex" value="male">Male</li>
+							<li><input type="radio" name="sex" value="male">Male</li>
+							<li><input type="radio" name="sex" value="male">Male</li>
+							<li><input type="radio" name="sex" value="male">Male</li>
+						</ul>
+					</form>
+				</li>
+				<br>
+				<li>Siapakah nama saya ?
+					<form action="" method="post">
+						<ul>
+							<li><input type="radio" name="sex" value="male">Male</li>
+							<li><input type="radio" name="sex" value="male">Male</li>
+							<li><input type="radio" name="sex" value="male">Male</li>
+							<li><input type="radio" name="sex" value="male">Male</li>
+						</ul>
+					</form>
+				</li>
+				<br>
+				<li>Siapakah nama saya ?
+					<form action="" method="post">
+						<ul>
+							<li><input type="radio" name="sex" value="male">Male</li>
+							<li><input type="radio" name="sex" value="male">Male</li>
+							<li><input type="radio" name="sex" value="male">Male</li>
+							<li><input type="radio" name="sex" value="male">Male</li>
+						</ul>
+					</form>
+				</li>
+				<br>
+			</ol>
+			
+			</div>
+		  </div>
+		</div>
+	</div>
+	
+	<div class="row footer">
+		<div class="container footer">
+			<div class="col-md-12">Support by <a href="armenginestudio.com">ARMEngineStudio</a></div>
+		</div>
+	</div>
+
+</body>
+</html>
